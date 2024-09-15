3817755c760c9ee5c446464ddb9726859f15b443
Louis Blonde <loul0uboypy@gmail.com>
2024-09-15 19:10:29 +0200

    first function

--- site/app.js
+++ site/app.js
@@ -1,1 +1,28 @@
-console.log("Hello world !");
+const upper_0 = (word) => {
+  return word[0].toUpperCase() + word.substring(1, word.length).toLowerCase();
+};
+const createElement = (type, classer, value, index) => {
+  const e = document.createElement(type);
+  e.className = classer;
+  e.value = value;
+  e.id = index;
+  return e;
+};
+const tester = (questions) => {
+  result = "";
+  for (let i = 0; i < questions.length; i++) {
+    document
+      .querySelector(".inputs")
+      .appendChild(createElement("div", "test", "Hello", i));
+    x = Math.floor(Math.random() * questions.length);
+    element = questions[x].name;
+    console.log(`${upper_0(element)} ? `);
+  }
+  return result;
+};
+console.log(
+  tester([
+    { name: "test", trad: "tester" },
+    { name: "hola", trad: "hello" },
+  ])
+);
