478c9902fe5b31a0b42fb5ab4e5481ed604ba089
Louis Blonde <loul0uboypy@gmail.com>
2024-09-15 19:12:12 +0200

    good function

--- python/app.py
+++ python/app.py
@@ -2,33 +2,33 @@ import random
 import json
 import math
 
-def save_questions(element, name):
+def lb_save_questions(element, name):
     file = open(f'./{name}.json', 'w')
     file.write(json.dumps(element))
     file.close()
     return
-def read_questions():
+def lb_read_questions():
     path = './' + input('File\'s name : ') + '.json'
     file = open(path, 'r')
     reader = file.read()
     file.close()
     return json.loads(reader)
-def create_list(questions):
+def lb_create_list(questions):
     new_test = input('WORD:TRADUCTION >>> ')
     if new_test.lower() != 'none' and new_test.lower() != 'exit':
         new_word = new_test[0:new_test.index(':')]
         new_trad = new_test[new_test.index(':') + 1:len(new_test)]
         questions.update({f'{new_word}': f'{new_trad}'})
-        create_list(questions)
+        lb_create_list(questions)
     else:
         save = input('Save questions ? [y/n] ')
         if save.lower() == 'y' or save.lower() == 'yes':
             file_name = input('File\'s name : ')
-            save_questions(questions, file_name)
+            lb_save_questions(questions, file_name)
     return questions
-def upper_0(word):
+def lb_upper_0(word):
     return word[0].upper() + word[1:len(word)].lower()
-def tester(questions):
+def lb_tester(questions):
     note = 0
     evaluator = questions.copy()
     result = ['\n']
@@ -39,28 +39,28 @@ def tester(questions):
         response = input(f'{element[0].upper() + element[1:len(element)].lower()} ? ')
         if response == evaluator.get(element.lower()) and evaluator.get(element.lower()):
             print('\033[1;92mCorrect\033[0m')
-            result.append(f'\033[1;92m{upper_0(element)} : {upper_0(questions.get(element))}\033[0m')
+            result.append(f'\033[1;92m{lb_upper_0(element)} : {lb_upper_0(questions.get(element))}\033[0m')
             note += 1
         else:
             print('\033[1;31mIncorrect\033[0m')
-            result.append(f'\033[1;31m{upper_0(element)} : {questions.get(element)}\033[0m')
+            result.append(f'\033[1;31m{lb_upper_0(element)} : {questions.get(element)}\033[0m')
         del evaluator[element]
         i += 1
     for i in result:
         print(i)
     print(f'\033[1;34m{note}/{len(questions)}\033[0m')
-    defaulter()
-def defaulter():
+    lb_defaulter()
+def lb_defaulter():
     todo = input('>>> ')
     if todo == 'create':
-        create_list({})
-        defaulter()
+        lb_create_list({})
+        lb_defaulter()
     elif todo == 'test':
-        tester(read_questions())
-        defaulter()
+        lb_tester(lb_read_questions())
+        lb_defaulter()
     elif todo == 'exit' or todo == 'quit' or todo == 'q':
         exit()
     else:
         print(f'\'{todo}\' is not a command.')
-        defaulter()
-defaulter()
+        lb_defaulter()
+lb_defaulter()
--- site/app.js
+++ site/app.js
@@ -1,14 +1,14 @@
-const upper_0 = (word) => {
+const lb_upper_0 = (word) => {
   return word[0].toUpperCase() + word.substring(1, word.length).toLowerCase();
 };
-const createElement = (type, classer, value, index) => {
+const lb_createElement = (type, classer, value, index) => {
   const e = document.createElement(type);
   e.className = classer;
   e.value = value;
   e.id = index;
   return e;
 };
-const tester = (questions) => {
+const lb_tester = (questions) => {
   result = "";
   for (let i = 0; i < questions.length; i++) {
     document
@@ -16,12 +16,12 @@ const tester = (questions) => {
       .appendChild(createElement("div", "test", "Hello", i));
     x = Math.floor(Math.random() * questions.length);
     element = questions[x].name;
-    console.log(`${upper_0(element)} ? `);
+    console.log(`${lb_upper_0(element)} ? `);
   }
   return result;
 };
 console.log(
-  tester([
+  lb_tester([
     { name: "test", trad: "tester" },
     { name: "hola", trad: "hello" },
   ])
