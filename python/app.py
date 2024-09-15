344188dfd81f98c508ac09130c002014c7abb415
Louis Blonde <loul0uboypy@gmail.com>
2024-09-15 11:56:18 +0200

    remove secure && add upper index 0

--- python/app.py
+++ python/app.py
@@ -1,24 +1,7 @@
 import random
 import json
-import string
+import math
 
-alphabet = '    ' + ' ' + string.punctuation + string.ascii_letters + string.digits
-def crypter(message, key, direction=1):
-    key_index = 0
-    encrypted_message = ''
-    for char in message:
-        key_char = key[key_index % len(key)]
-        key_index += 1
-        offset = alphabet.index(key_char)
-        index = alphabet.find(char)
-        new_index = (index + offset*direction) % len(alphabet)
-        encrypted_message += alphabet[new_index]
-    return encrypted_message
-
-def decrypt(message, key):
-    return crypter(message, key, -1)
-def encrypt(message, key):
-    return crypter(message, key)
 def save_questions(element, name):
     file = open(f'./{name}.json', 'w')
     file.write(json.dumps(element))
@@ -36,46 +19,37 @@ def create_list(questions):
         new_word = new_test[0:new_test.index(':')]
         new_trad = new_test[new_test.index(':') + 1:len(new_test)]
         questions.update({f'{new_word}': f'{new_trad}'})
-        print(questions)
         create_list(questions)
     else:
         save = input('Save questions ? [y/n] ')
         if save.lower() == 'y' or save.lower() == 'yes':
             file_name = input('File\'s name : ')
-            passw = input('File\'s password : ')
-            new_questions = {'code_path': encrypt('lbcode', passw)}
-            for i in questions:
-                new_questions.update({f'{i}': f'{encrypt(questions[i], passw)}'})
-            save_questions(new_questions, file_name)
+            save_questions(questions, file_name)
     return questions
+def upper_0(word):
+    return word[0].upper() + word[1:len(word)].lower()
 def tester(questions):
-    passw = input('File\'s password : ')
-    print(questions['code_path'])
-    if decrypt(questions['code_path'], passw) == 'lbcode':
-        note = 0
-        evaluator = questions.copy()
-        result = ['\n']
-        i = 0
-        while i < len(questions) - 1:
-            x = random.randint(1, len(evaluator) - 1)
-            print(len(evaluator), x)
-            element = list(evaluator.keys())[x]
-            response = input(f'{element[0].upper() + element[1:len(element)].lower()} ? ')
-            if response == decrypt(evaluator.get(element.lower()), passw) and evaluator.get(element.lower()):
-                print('\033[1;92mCorrect\033[0m')
-                result.append(f'\033[1;92m{element} : {decrypt(questions.get(element), passw)}\033[0m')
-                note += 1
-            else:
-                print('\033[1;31mIncorrect\033[0m')
-                result.append(f'\033[1;31m{element} : {decrypt(questions.get(element), passw)}\033[0m')
-            del evaluator[element]
-            i += 1
-        for i in result:
-            print(i)
-        print(f'\033[1;34m{note}/{len(questions) - 1}\033[0m')
-    else:
-        print('Wrong password.')
-        defaulter()
+    note = 0
+    evaluator = questions.copy()
+    result = ['\n']
+    i = 0
+    while i < len(questions):
+        x = math.floor(random.random() * len(evaluator))
+        element = list(evaluator.keys())[x]
+        response = input(f'{element[0].upper() + element[1:len(element)].lower()} ? ')
+        if response == evaluator.get(element.lower()) and evaluator.get(element.lower()):
+            print('\033[1;92mCorrect\033[0m')
+            result.append(f'\033[1;92m{upper_0(element)} : {upper_0(questions.get(element))}\033[0m')
+            note += 1
+        else:
+            print('\033[1;31mIncorrect\033[0m')
+            result.append(f'\033[1;31m{upper_0(element)} : {questions.get(element)}\033[0m')
+        del evaluator[element]
+        i += 1
+    for i in result:
+        print(i)
+    print(f'\033[1;34m{note}/{len(questions)}\033[0m')
+    defaulter()
 def defaulter():
     todo = input('>>> ')
     if todo == 'create':
@@ -85,7 +59,7 @@ def defaulter():
         tester(read_questions())
         defaulter()
     elif todo == 'exit' or todo == 'quit' or todo == 'q':
-        print('\nGood bye.')
+        exit()
     else:
         print(f'\'{todo}\' is not a command.')
         defaulter()
